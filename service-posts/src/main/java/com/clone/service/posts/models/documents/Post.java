4dd8cbf10b58708d270e8c4642c2bb075926d99e
Cristian Villanova <cristiandavidv@hotmail.com>
2023-04-14 17:27:33 -0500

    Changes config files

--- service-posts/src/main/java/com/clone/service/posts/models/documents/Post.java
+++ service-posts/src/main/java/com/clone/service/posts/models/documents/Post.java
@@ -5,8 +5,11 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.util.List;
 
 @Document(collection = "posts")
 @Data
@@ -18,4 +21,6 @@ public class Post {
     private String description;
     @Field("sub_category_id")
     private Long subCategoryId;
+    @Transient
+    List<File> files;
 }
